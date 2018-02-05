fa14d360075a627373a215d3d83cc2303de1342f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-02-05 15:55:51 -0300

    fixed Pure-C

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
-    builder.add_common_builds(shared_option_name="libqrencode:shared", pure_c=False)
+    builder.add_common_builds(shared_option_name="libqrencode:shared", pure_c=True)
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
